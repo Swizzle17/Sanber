e81b52b78c92d82788985f9e750f5bd74825f828
Swizzle17 <irenecindy1704@gmail.com>
2023-06-09 15:45:07 +0700

    Add javascript demo sanber 45

---
+++ strukDat.js
@@ -0,0 +1,18 @@
+//Array
+//Object
+
+var mobil = ["Avanza", "Yaris", "Alya"]
+//Avanza adalah element mobil ke 0
+//Yaris adalah element mobil ke 1
+//Alya adalah element mobil ke 2
+console.log(mobil)
+
+var dataKendaraan = ["Ujang",24,"Avanza","B 1234 AGG",6000000,false]
+
+//Menambahkan element ke dalam mobil
+mobil.push("Alphard")
+console.log(mobil)
+
+dataKendaraan2 = []
+console.log(dataKendaraan2)
+
